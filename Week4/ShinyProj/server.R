3ba19614876cb126ce4ba58d804cdd0249d09128
Tiago Berti <Tiago Berti>
2017-08-22 21:13:14 -0300

    2nd commit

--- Week4/ShinyProj/server.R
+++ Week4/ShinyProj/server.R
@@ -1,18 +1,9 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(ggplot2)
 library(datasets)
 data(Orange)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
   mylm <- reactive({
@@ -26,10 +17,8 @@ shinyServer(function(input, output) {
    
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
     x    <- Orange$age 
     SelectedTree <- input$Tree
-    # draw the histogram with the specified number of bins
     OrangeSub <- Orange[Orange$Tree==SelectedTree,]
     mymodel <- mylm()
     p11 <- ggplot(OrangeSub, aes(x=age, y=circumference)) + geom_point(shape=1) + geom_smooth(method=lm)
--- Week4/ShinyProj/ui.R
+++ Week4/ShinyProj/ui.R
@@ -1,21 +1,12 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a scatterplot with the regression line subset by Tree
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Orange Data"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a radio button input for Tree
   sidebarLayout(
     sidebarPanel(
       radioButtons("Tree", label = h3("Tree Identification"),
@@ -23,7 +14,7 @@ shinyUI(fluidPage(
                                   "Tree 3" = 3,"Tree 4" = 4, "Tree 5" = 5),selected = 1
                    )),
     
-    # Show a plot of the generated distribution
+    # Show a plot of the data and linear regression
     mainPanel(
       h3("Linear Model:"),
       plotOutput("distPlot"))
